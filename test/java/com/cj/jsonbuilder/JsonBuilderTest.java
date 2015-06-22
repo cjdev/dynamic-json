88aaae0dc111c0a219e302086a0b66bceef2a1f0
Anthony Coccia <acoccia@cj.com>
2015-06-22 16:32:54 -0700

    Initial project structure.

---
+++ test/java/com/cj/jsonbuilder/JsonBuilderTest.java
@@ -0,0 +1,12 @@
+package com.cj.jsonbuilder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JsonBuilderTest {
+    @Test
+    public void testSimpleJsoninification(){
+        assertTrue(true);
+    }
+}
